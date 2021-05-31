26e2392194920faec806ccb0e8d3b688fd76d390
Tia <mattiagiacom3llo@gmail.com>
2021-05-31 19:14:41 +0200

    adds call

--- wip/vuemmerce/pages/index.vue
+++ wip/vuemmerce/pages/index.vue
@@ -8,12 +8,20 @@
 <script>
 import VmProductsList from '@/components/products_list/ProductsListContainer';
 import VmHero from '@/components/hero/Hero';
+import axios from 'axios'
 
 export default {
   name: 'index',
   components: {
     VmProductsList,
     VmHero
+  },
+
+  created(){
+    axios.get('http://127.0.0.1/api/products.php')
+        .then((response) => {
+            this.$store.state.products = response.data;
+        })
   }
 };
 </script>
